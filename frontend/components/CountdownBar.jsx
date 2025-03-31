b6da26aeec03ace3b82624dd7a29ba820339c5c6
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-03-31 17:10:43 +1100

    Change countdown bar styling

--- frontend/components/CountdownBar.jsx
+++ frontend/components/CountdownBar.jsx
@@ -1,9 +1,25 @@
 import '../styles/CountdownBar.css';
 
 const CountdownBar = ({ remainingPercent }) => {
+  const getColour = () => {
+    let r = 200;
+    let g = 200;
+
+    if (remainingPercent > 50) {
+      r *= 2 * (1 - remainingPercent / 100);
+    } else {
+      g *= 2 * (remainingPercent / 100);
+    }
+
+    return `rgb(${r}, ${g}, 0)`;
+  };
+
   return (
     <div className="countdown-bar">
-      <div className="remaining" style={{ height: `${remainingPercent}%` }}></div>
+      <div
+        className="remaining"
+        style={{ height: `${remainingPercent}%`, backgroundColor: getColour() }}
+      ></div>
     </div>
   );
 };
