6711205276388872ef09bd5f0512f3380fa86502
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-04-01 11:31:33 +1100

    Add neon glow effect to countdown bar

--- frontend/components/CountdownBar.jsx
+++ frontend/components/CountdownBar.jsx
@@ -1,24 +1,34 @@
 import '../styles/CountdownBar.css';
 
 const CountdownBar = ({ remainingPercent }) => {
+  const getBoxShadow = () => {
+    return `
+      0 0 0.1vw ${getColour()},
+      0 0 0.2vw ${getColour()},
+      0 0 0.4vw ${getColour()},
+      0 0 0.8vw ${getColour()},
+      0 0 1.6vw ${getColour()},
+      0 0 3.2vw ${getColour()},
+      0 0 6.4vw ${getColour()},
+      0 0 12.8vw ${getColour()},
+      0 0 25.6vw ${getColour()},
+      0 0 51.2vw ${getColour()}`;
+  };
+
   const getColour = () => {
-    let r = 200;
-    let g = 200;
-
-    if (remainingPercent > 50) {
-      r *= 2 * (1 - remainingPercent / 100);
-    } else {
-      g *= 2 * (remainingPercent / 100);
-    }
-
-    return `rgb(${r}, ${g}, 0)`;
+    const hue = 120 * (remainingPercent / 100);
+    return `hsl(${hue}, 80%, 50%)`;
   };
 
   return (
     <div className="countdown-bar">
       <div
         className="remaining"
-        style={{ height: `${remainingPercent}%`, backgroundColor: getColour() }}
+        style={{
+          height: `${remainingPercent}%`,
+          backgroundColor: 'rgb(205, 205, 205)',
+          boxShadow: getBoxShadow(),
+        }}
       ></div>
     </div>
   );
