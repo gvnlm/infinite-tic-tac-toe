94f2f8b92aa91e52f92c6ec669feebd322b925dd
gvnlm <150659730+gvnlm@users.noreply.github.com>
2025-07-30 18:35:52 +1000

    Add link to GitHub repo on home screen

--- frontend/components/Home.jsx
+++ frontend/components/Home.jsx
@@ -1,15 +1,26 @@
 import BackgroundGame from '../components/BackgroundGame';
 import GameModeButton from '../components/GameModeButton';
+import { FaGithub } from 'react-icons/fa';
+
 import '../styles/Home.css';
+
+const GITHUB_REPO_LINK = 'https://github.com/gvnlm/infinite-tic-tac-toe';
+const GITHUB_USER = 'gvnlm';
 
 const Home = ({ onClickAI, onClickLocalMultiplayer }) => {
   return (
     <div className="home">
       <BackgroundGame />
+
       <div className="game-mode-buttons">
         <GameModeButton text="AI" onClick={onClickAI} />
         <GameModeButton text="Local Multiplayer" onClick={onClickLocalMultiplayer} />
       </div>
+
+      <a className="github-link" href={GITHUB_REPO_LINK}>
+        <FaGithub />
+        {GITHUB_USER}
+      </a>
     </div>
   );
 };
